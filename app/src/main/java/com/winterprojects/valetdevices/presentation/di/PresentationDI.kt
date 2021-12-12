49d0aeaaa1789d2904ca2d9f3f9a6ce09bff2de0
cristiano.winter <cristiano.winter@hotmail.com>
2021-12-12 12:37:12 -0300

    Added DeviceDetailsViewModel.kt and Unit Test Updated DeviceDetailsFragment.kt to keep state using view model Fix wrong injection delegate on DevicesFragment.kt

--- app/src/main/java/com/winterprojects/valetdevices/presentation/di/PresentationDI.kt
+++ app/src/main/java/com/winterprojects/valetdevices/presentation/di/PresentationDI.kt
@@ -1,5 +1,7 @@
 package com.winterprojects.valetdevices.presentation.di
 
+import com.winterprojects.valetdevices.domain.devices.models.DeviceModel
+import com.winterprojects.valetdevices.presentation.deviceDetails.DeviceDetailsViewModel
 import com.winterprojects.valetdevices.presentation.devices.DevicesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,6 +10,10 @@ object PresentationDI {
     val module = module {
         viewModel {
             DevicesViewModel(get())
+        }
+
+        viewModel { (device: DeviceModel) ->
+            DeviceDetailsViewModel(device)
         }
     }
 }
---
+++ app/src/test/java/com/winterprojects/valetdevices/presentation/devices/DeviceDetailsViewModelTest.kt
@@ -0,0 +1,65 @@
+package com.winterprojects.valetdevices.presentation.devices
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.winterprojects.valetdevices.business.devices.GetAllDevicesUseCase
+import com.winterprojects.valetdevices.domain.devices.models.DeviceModel
+import com.winterprojects.valetdevices.helpers.BaseTest
+import com.winterprojects.valetdevices.helpers.StateResult
+import com.winterprojects.valetdevices.helpers.getDeviceModelTest
+import com.winterprojects.valetdevices.presentation.deviceDetails.DeviceDetailsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.koin.test.mock.declareMock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.notNull
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+class DeviceDetailsViewModelTest : BaseTest() {
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private lateinit var deviceDetailsViewModel: DeviceDetailsViewModel
+
+    private lateinit var observer: Observer<DeviceModel>
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        Dispatchers.setMain(TestCoroutineDispatcher())
+
+        observer = declareMock()
+    }
+
+    @Test
+    fun `should init state with a model data`() = runBlocking {
+        //Arrange
+        deviceDetailsViewModel = DeviceDetailsViewModel(getDeviceModelTest())
+
+        //Act
+        deviceDetailsViewModel.deviceLiveData.observeForever(observer)
+
+        //Assert
+        verify(observer).onChanged(notNull())
+    }
+
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+}
